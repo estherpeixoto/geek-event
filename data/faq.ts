fb2755517ba75a5a37e14a94a018471dd996a70b
Esther Peixoto <estherpeixoto13@gmail.com>
2022-08-14 14:03:10 -0300

    pagination

--- data/faq.ts
+++ data/faq.ts
@@ -12,5 +12,12 @@ export const faq = {
     { id: '3', question: 'Third question', answer: 'Third answer' },
     { id: '4', question: 'Fourth question', answer: 'Fourth answer' },
     { id: '5', question: 'Fifth question', answer: 'Fifth answer' },
+    { id: '6', question: 'Rest question', answer: 'Rest answer' },
+    { id: '7', question: 'Rest question', answer: 'Rest answer' },
+    { id: '8', question: 'Rest question', answer: 'Rest answer' },
+    { id: '9', question: 'Rest question', answer: 'Rest answer' },
+    { id: '10', question: 'Rest question', answer: 'Rest answer' },
+    { id: '11', question: 'Rest question', answer: 'Rest answer' },
+    { id: '12', question: 'Rest question', answer: 'Rest answer' },
   ],
 }
--- pages/admin/faq/index.tsx
+++ pages/admin/faq/index.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 // Data
 import { faq, FAQType } from 'data/faq'
+
+// Utils
 import { classNames } from 'utils/functions'
 
 // Layout
@@ -11,10 +14,18 @@ import { Admin } from 'layouts/Admin'
 // Components
 import { CreateButton, Description } from 'components/commons'
 import { Actions, actionsWidth } from 'components/Admin/Table/Actions'
+import { Pagination } from 'components/Admin/Table/Pagination'
 
 const FAQ: NextPage = () => {
+  const router = useRouter()
+
   const [list, setList] = useState<FAQType[]>([])
   const [search, setSearch] = useState('')
+
+  const rowsPerPage = 5
+  const currentPage = parseInt((router.query.p as string) ?? 1)
+
+  const offset = (currentPage - 1) * rowsPerPage
 
   useEffect(() => {
     setList(faq.data)
@@ -25,7 +36,7 @@ const FAQ: NextPage = () => {
       return Object.values(item)
         .join('')
         .toLowerCase()
-        .includes(search.toLowerCase())
+        .includes(search.trim().toLowerCase())
     })
 
     setList(filteredData)
@@ -66,7 +77,10 @@ const FAQ: NextPage = () => {
         </thead>
 
         <tbody>
-          {list.map((question, index) => {
+          {(search.trim() == ''
+            ? list.slice(offset, offset + rowsPerPage)
+            : list
+          ).map((question, index) => {
             return (
               <tr key={index} className="text-gray-500">
                 <td className="p-4">{question.id}</td>
@@ -82,6 +96,18 @@ const FAQ: NextPage = () => {
             )
           })}
         </tbody>
+
+        <tfoot>
+          <tr>
+            <td colSpan={3}>
+              <Pagination
+                rowsPerPage={rowsPerPage}
+                currentPage={currentPage}
+                contentLength={list.length}
+              />
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </Admin>
   )
