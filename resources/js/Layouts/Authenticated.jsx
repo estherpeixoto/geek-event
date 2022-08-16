2a1a4c16b1ba4698aa2254a0d58b194196ae617b
Esther Peixoto <37677602+estherpeixoto@users.noreply.github.com>
2022-08-16 12:50:06 -0300

    change light colors to dark

--- resources/js/Layouts/Authenticated.jsx
+++ resources/js/Layouts/Authenticated.jsx
@@ -22,7 +22,7 @@ export default function Authenticated({ auth, header, children }) {
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="bg-gray-800 border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto px-4 lg:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex items-center">
                             <div className="shrink-0 flex items-center mr-10">
@@ -31,40 +31,33 @@ export default function Authenticated({ auth, header, children }) {
                                 </Link>
                             </div>
 
-                            <div className='flex items-baseline space-x-4'>
-                                {adminNavigation.map((item) => {
-                                    return (
-                                        <NavLink key={item.route} href={route(item.route)} active={route().current(item.route)}>
-                                            {item.name}
-                                        </NavLink>
-                                    )
-                                })}
+                            <div className="hidden lg:block">
+                                <div className='flex items-baseline space-x-4'>
+                                    {adminNavigation.map((item) => {
+                                        return (
+                                            <NavLink key={item.route} href={route(item.route)} active={route().current(item.route)}>
+                                                {item.name}
+                                            </NavLink>
+                                        )
+                                    })}
+                                </div>
                             </div>
                         </div>
 
-                        <div className="hidden sm:flex sm:items-center sm:ml-6">
+                        <div className="hidden lg:flex lg:items-center lg:ml-6">
                             <div className="ml-3 relative">
                                 <Dropdown>
                                     <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
+                                        <span className="flex items-center rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                className="h-8 w-8 inline-flex items-center focus:outline-none transition ease-in-out duration-150 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-full bg-gray-800"
                                             >
-                                                {auth.user.name}
-
-                                                <svg
-                                                    className="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
+                                                <img
+                                                    className="h-8 w-8 rounded-full"
+                                                    src='https://avatars.githubusercontent.com/u/37677602?v=4'
+                                                    alt={auth.user.name}
+                                                />
                                             </button>
                                         </span>
                                     </Dropdown.Trigger>
@@ -78,10 +71,10 @@ export default function Authenticated({ auth, header, children }) {
                             </div>
                         </div>
 
-                        <div className="-mr-2 flex items-center sm:hidden">
+                        <div className="-mr-2 flex items-center lg:hidden">
                             <button
                                 onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                             >
                                 <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path
@@ -104,17 +97,21 @@ export default function Authenticated({ auth, header, children }) {
                     </div>
                 </div>
 
-                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
+                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' lg:hidden'}>
                     <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
-                            Dashboard
-                        </ResponsiveNavLink>
+                        {adminNavigation.map((item) => {
+                            return (
+                                <ResponsiveNavLink key={item.route} href={route(item.route)} active={route().current(item.route)}>
+                                    {item.name}
+                                </ResponsiveNavLink>
+                            )
+                        })}
                     </div>
 
-                    <div className="pt-4 pb-1 border-t border-gray-200">
+                    <div className="pt-4 pb-1 border-t border-gray-700">
                         <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">{auth.user.name}</div>
-                            <div className="font-medium text-sm text-gray-500">{auth.user.email}</div>
+                            <div className="font-medium text-base text-gray-300">{auth.user.name}</div>
+                            <div className="font-medium text-sm text-gray-400">{auth.user.email}</div>
                         </div>
 
                         <div className="mt-3 space-y-1">
@@ -128,7 +125,7 @@ export default function Authenticated({ auth, header, children }) {
 
             {header && (
                 <header className="bg-white shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
+                    <div className="max-w-7xl mx-auto py-6 px-4 lg:px-6 lg:px-8">{header}</div>
                 </header>
             )}
 
